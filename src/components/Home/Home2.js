1d7a906b46dfc8b00b9cc48812c6541a111e0eaa
dkswami <dkswami@gmail.com>
2023-01-07 17:48:43 +0530

    footer updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -73,7 +73,7 @@ function Home2() {
 							</li>
 							{/* <li className="social-icons">
 								<a
-									href="https://twitter.com/Soumyajit4419"
+									href="https://twitter.com/"
 									target="_blank"
 									rel="noreferrer"
 									className="icon-colour  home-social-icons"
