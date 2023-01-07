02594acaac3fa5eb3ef229099f14d91ddb0d3049
dkswami <dkswami@gmail.com>
2023-01-07 17:42:43 +0530

    about page updated

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,54 +2,83 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
+	DiJavascript1,
+	DiReact,
+	DiNodejs,
+	DiMongodb,
+	DiPython,
+	DiGit,
 } from "react-icons/di";
 import {
-  SiPytorch,
-  SiFirebase,
-  SiNextdotjs,
+	SiHtml5,
+	SiCss3,
+	SiExpress,
+	SiFirebase,
+	SiNextdotjs,
+	SiNestjs,
+	SiRedux,	
+	SiGraphql,
+	SiBootstrap,
+	SiMaterialui
 } from "react-icons/si";
 
+
 function Techstack() {
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
-      </Col>
-    </Row>
-  );
+	return (
+		<Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiHtml5 />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiCss3 />
+			</Col>			
+			<Col xs={4} md={2} className="tech-icons">
+				<DiJavascript1 />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiNodejs />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiReact />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiMongodb />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiExpress />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<CgCPlusPlus />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiNextdotjs />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiGit />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiFirebase />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<DiPython />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiNestjs />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiRedux />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiGraphql />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiBootstrap />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiMaterialui />
+			</Col>
+		</Row>
+	);
 }
 
 export default Techstack;
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,33 +1,47 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiLinux,
-  SiVisualstudiocode,
-  SiPostman,
-  SiHeroku,
-  SiVercel,
+	SiLinux,
+	SiVisualstudiocode,
+	SiPostman,
+	SiHeroku,
+	SiVercel,
+	SiWebstorm
 } from "react-icons/si";
+import { 
+	FaAws,
+	FaDocker,
+} from "react-icons/fa";
 
 function Toolstack() {
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHeroku />
-      </Col>
-    </Row>
-  );
+	return (
+		<Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiLinux />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiVisualstudiocode />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiWebstorm />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiPostman />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiVercel />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<SiHeroku />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<FaAws />
+			</Col>
+			<Col xs={4} md={2} className="tech-icons">
+				<FaDocker />
+			</Col>
+		</Row>
+	);
 }
 
 export default Toolstack;
